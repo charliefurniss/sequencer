6269177faf0aac4dfbb2cb495bd26d7f7ca49e99
Charlie Furniss <charliefurniss96@gmail.com>
2016-07-07 14:58:02 +0100

    adding stop button functionality

--- js/webaudio.js
+++ js/webaudio.js
@@ -1,81 +1,94 @@
-window.onload = init;
-var context;
-var bufferLoader;
-var RhythmSample = {
-};
+$(document).ready(function() {
 
-function init() {
-  // Fix up prefixing
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  context = new AudioContext();
+	window.onload = init;
+	var context;
+	var bufferLoader;
+	var RhythmSample = {
+	};
 
-  bufferLoader = new BufferLoader(
-    context,
-    [
-      'audio/Kick909.wav',
-      'audio/Snare909.wav',
-      'audio/MA808.wav',
-      'audio/TomSim1.wav'
-    ],
-    RhythmSample.play
-   );
+	function init() {
+	  // Fix up prefixing
+	  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	  context = new AudioContext();
 
-  bufferLoader.load();
-}
+	  bufferLoader = new BufferLoader(
+	    context,
+	    [
+	      'audio/Kick909.wav',
+	      'audio/Snare909.wav',
+	      'audio/MA808.wav',
+	      'audio/TomSim1.wav'
+	    ],
+	    setUpStart
+	   );
 
-RhythmSample.play = function(bufferList) {
-	console.log(bufferList);
-  function playSound(buffer, time) {
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    if (!source.start)
-      source.start = source.noteOn;
-    source.start(time);
-  }
+	  bufferLoader.load();
+	}
 
-  var kick = bufferList[0];
-  var snare = bufferList[1];
-  var hihat = bufferList[2];
-  var wave = bufferList[3];
+	function setUpStart(bufferList){
+		$('#playButton').click(function(){
+		  RhythmSample.play(bufferList);
+		});
+	}
 
-  // We'll start playing the rhythm 100 milliseconds from "now"
-  var startTime = context.currentTime + 0.100;
-  var tempo = 110; // BPM (beats per minute)
-  var sixteenthNoteTime = (60 / tempo) / 4;
+	RhythmSample.play = function(bufferList) {
+	
+	  function playSound(buffer, time) {
+	    var source = context.createBufferSource();
+	    source.buffer = buffer;
+	    source.connect(context.destination);
+	    if (!source.start)
+	      source.start = source.noteOn;
+	    source.start(time);
+	    $('#stopButton').click(function(){
+	      source.stop(0);
+	    });
+	  }
 
-  var loopLength = 2;
-  var seqLength = 4;
+	  var kick = bufferList[0];
+	  var snare = bufferList[1];
+	  var hihat = bufferList[2];
+	  var wave = bufferList[3];
 
-  var kickArray = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0];
-  var snareArray = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
-  var hihatArray = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+	  // We'll start playing the rhythm 100 milliseconds from "now"
+	  var startTime = context.currentTime + 0.100;
+	  var tempo = 110; // BPM (beats per minute)
+	  var sixteenthNoteTime = (60 / tempo) / 4;
 
-  for (var seq = 0; seq < seqLength; seq=seq+loopLength) {
+	  var loopLength = 2;
+	  var seqLength = 4;
 
-    var time = startTime + seq * 16 * sixteenthNoteTime;
-    
-    for (var kickSeq=0; kickSeq < kickArray.length; kickSeq++){
-    	if (kickArray[kickSeq] == 1){
-    		playSound(kick, time + kickSeq * sixteenthNoteTime);
-    	}	
-    }
-    
-    for (var snareSeq=0; snareSeq < snareArray.length; snareSeq++){
-    	if (snareArray[snareSeq] == 1){
-    		playSound(snare, time + snareSeq * sixteenthNoteTime);
-    	}	
-    }
+	  var kickArray = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0];
+	  var snareArray = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+	  var hihatArray = [1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0];
 
-    for (var hihatSeq=0; hihatSeq < hihatArray.length; hihatSeq++){
-    	if (hihatArray[hihatSeq] == 1){
-    		playSound(hihat, time + hihatSeq * sixteenthNoteTime);
-    	}    
-    }
+	  for (var seq = 0; seq < seqLength; seq=seq+loopLength) {
 
-    // playSound(wave, time);
-  }
-};
+	    var time = startTime + seq * 16 * sixteenthNoteTime;
+	    
+	    for (var kickSeq=0; kickSeq < kickArray.length; kickSeq++){
+	    	if (kickArray[kickSeq] == 1){
+	    		playSound(kick, time + kickSeq * sixteenthNoteTime);
+	    	}	
+	    }
+	    
+	    for (var snareSeq=0; snareSeq < snareArray.length; snareSeq++){
+	    	if (snareArray[snareSeq] == 1){
+	    		playSound(snare, time + snareSeq * sixteenthNoteTime);
+	    	}	
+	    }
+
+	    for (var hihatSeq=0; hihatSeq < hihatArray.length; hihatSeq++){
+	    	if (hihatArray[hihatSeq] == 1){
+	    		playSound(hihat, time + hihatSeq * sixteenthNoteTime);
+	    	}    
+	    }
+
+	    // playSound(wave, time);
+	  }
+	};
+
+});
 
 
 
