80c6e6377e829dcbcb7c4e80f7883d099c7fc45a
Charlie Furniss <charliefurniss96@gmail.com>
2016-07-08 14:16:33 +0100

    added Sequence object

---
+++ js/sequence.js
@@ -0,0 +1,32 @@
+var Sequence = function(context){
+
+  this.play = function(bufferList, config, channelArray) {
+
+    function playSound(buffer, time) {
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      if (!source.start)
+        source.start = source.noteOn;
+      source.start(time);
+      $('#stopButton').click(function(){
+        source.stop(0);
+      });
+    }
+
+    var startTime = context.currentTime + 0.100;
+
+    for (var seq = 0; seq < config.seqLength; seq = seq + config.loopLength) {
+      var time = startTime + seq * 16 * config.sixteenthNoteTime;
+      for (i = 0; i < channelArray.length; i++){
+      	for (var step=0; step < channelArray[i].seqArray.length; step++){
+      		//play sound on steps with value of 1
+      		if (channelArray[i].seqArray[step] == 1){
+      			playSound(channelArray[i].instr, time + step * config.sixteenthNoteTime);
+      		}
+      	}
+      }
+    }
+  };
+
+}
--- js/webaudio.js
+++ js/webaudio.js
@@ -6,9 +6,8 @@ $(document).ready(function() {
 	var bufferLoader;
 	var sequencer;
 	var channelArray;	
-	var RhythmSample = {
-	};
 	
+	var sequence;
 	var config = new Config();
 
 	function init() {
@@ -16,6 +15,7 @@ $(document).ready(function() {
 		context	= new Context();
 		context = context.context;
 		sequencer = new Sequencer(context);
+		sequence = new Sequence(context);
 		channelArray = sequencer.channelArray;
 
 	  bufferLoader = new BufferLoader(
@@ -37,40 +37,11 @@ $(document).ready(function() {
 
 	function setUp(bufferList){
 		$('#playButton').click(function(){
-		  RhythmSample.play(bufferList);
+		  sequence.play(bufferList, config, channelArray);
 		});
 		sequencer.setUpChannels(bufferList);
 		sequencer.registerSeqButtonClick();
 	}
-
-	RhythmSample.play = function(bufferList) {
-	
-	  function playSound(buffer, time) {
-	    var source = context.createBufferSource();
-	    source.buffer = buffer;
-	    source.connect(context.destination);
-	    if (!source.start)
-	      source.start = source.noteOn;
-	    source.start(time);
-	    $('#stopButton').click(function(){
-	      source.stop(0);
-	    });
-	  }
-
-	  var startTime = context.currentTime + 0.100;
-
-	  for (var seq = 0; seq < config.seqLength; seq = seq + config.loopLength) {
-	    var time = startTime + seq * 16 * config.sixteenthNoteTime;
-	    for (i = 0; i < channelArray.length; i++){
-	    	for (var step=0; step < channelArray[i].seqArray.length; step++){
-	    		//play sound on steps with value of 1
-	    		if (channelArray[i].seqArray[step] == 1){
-	    			playSound(channelArray[i].instr, time + step * config.sixteenthNoteTime);
-	    		}
-	    	}
-	    }
-	  }
-	};
 
 });
 
